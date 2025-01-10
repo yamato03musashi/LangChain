e82e984843b007fcf4c8e59f6195857bb2b978e8
sekine <nc0uhxa756@gmail.com>
2025-01-10 12:40:04 +0900

    RAGベース

---
+++ sample/6_handson/rag_base.py
@@ -0,0 +1,49 @@
+from langchain_community.document_loaders import GitLoader
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+
+def file_filter(file_path: str) -> bool:
+    return file_path.endswith(".mdx")
+
+# Gitからデータを取得
+loader = GitLoader(
+    clone_url="https://github.com/langchain-ai/langchain",
+    repo_path="langchain",
+    branch="master",
+    file_filter=file_filter,
+)
+
+docs = loader.load()
+
+print(len(docs))
+
+# データをベクトル化
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+db = Chroma.from_documents(docs, embeddings)
+
+prompt = ChatPromptTemplate.from_template('''\
+ 以下の文脈だけを踏まえて質問に回答してください。
+
+ 文脈: """
+ {context}
+ """
+
+ 質問: {question}
+''')
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+retriever = db.as_retriever()
+
+chain = {
+    "question": RunnablePassthrough(),
+    "context": retriever
+} | prompt | model | StrOutputParser()
+
+chain.invoke({"question": "LangChainとは？"})
+
+
